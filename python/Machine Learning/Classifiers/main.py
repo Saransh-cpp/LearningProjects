497e2272ba73281f8bcdf4109706fde1dfea6f7c
Saransh <saransh0701@gmail.com>
2021-01-30 18:45:43 +0530

    Made the program more dynamic

--- python/Machine Learning/Classifiers/main.py
+++ python/Machine Learning/Classifiers/main.py
@@ -9,12 +9,27 @@ labels = iris.target
 clf = KNeighborsClassifier()
 clf.fit(features, labels)
 
-preds = clf.predict([[1, 1, 1, 1]])
+SepalLength = int(input('Enter Sepal Length in cm\n'))
+SepalWidth = int(input('Enter Sepal Width in cm\n'))
+PetalLength = int(input('Enter Petal Length in cm\n'))
+PetalWidth = int(input('Enter Petal Length in cm\n'))
 
-print(iris.DESCR)
-print(preds)
+preds = clf.predict([[SepalLength, SepalWidth, PetalLength, PetalWidth]])
 
+# print(iris.DESCR)
+# print(preds)
 # print(features[0], labels[0])
+
+Info = 'The plant is'
+
+if preds == 0:
+    print(Info, 'Iris-Setosa')
+elif preds == 1:
+    print(Info, 'Iris-Versicolour')
+else:
+    print(Info, 'Iris-Virginica')
+
+
 
 
 
