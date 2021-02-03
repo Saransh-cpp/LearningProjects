5839c1e09744ebaf6881592f2f86647166e490f4
Saransh <saransh0701@gmail.com>
2021-02-03 20:44:32 +0530

    Working on 'colour' and 'pybamm'

--- python/Colour files/HCL.py
+++ python/Colour files/HCL.py
@@ -1,6 +1,9 @@
 import colour
 import numpy as np
 
+from colour.utilities import (as_float_array, from_range_1, to_domain_1,
+                              tsplit, tstack)
+
 
 def RGB_to_HCL(RGB, gamma=3, Y_0=100):
     R, G, B = colour.utilities.tsplit(RGB)
@@ -103,6 +106,78 @@ def HCL_to_RGB(HCL, gamma=3, Y_0=100):
 
     return RGB
 
+def RGB_to_HSL(RGB):
+    """
+    Converts from *RGB* colourspace to *HSL* colourspace.
 
-RGB = np.array([255, 0, 0])
-print(RGB_to_HCL(RGB))
+    Parameters
+    ----------
+    RGB : array_like
+        *RGB* colourspace array.
+
+    Returns
+    -------
+    ndarray
+        *HSL* array.
+
+    Notes
+    -----
+
+    +------------+-----------------------+---------------+
+    | **Domain** | **Scale - Reference** | **Scale - 1** |
+    +============+=======================+===============+
+    | ``RGB``    | [0, 1]                | [0, 1]        |
+    +------------+-----------------------+---------------+
+
+    +------------+-----------------------+---------------+
+    | **Range**  | **Scale - Reference** | **Scale - 1** |
+    +============+=======================+===============+
+    | ``HSL``    | [0, 1]                | [0, 1]        |
+    +------------+-----------------------+---------------+
+
+    References
+    ----------
+    :cite:`EasyRGBl`, :cite:`Smith1978b`, :cite:`Wikipedia2003`
+
+    Examples
+    --------
+    >>> RGB = np.array([0.45620519, 0.03081071, 0.04091952])
+    >>> RGB_to_HSL(RGB)  # doctest: +ELLIPSIS
+    array([ 0.9960394...,  0.8734714...,  0.2435079...])
+    """
+
+    RGB = to_domain_1(RGB)
+
+    minimum = np.amin(RGB, -1)
+    maximum = np.amax(RGB, -1)
+    delta = np.ptp(RGB, -1)
+
+    R, G, B = tsplit(RGB)
+
+    L = (maximum + minimum) / 2
+
+    S = np.where(
+        L < 0.5,
+        delta / (maximum + minimum),
+        delta / (2 - maximum - minimum),
+    )
+    S[np.asarray(delta == 0)] = 0
+
+    delta_R = (((maximum - R) / 6) + (delta / 2)) / delta
+    delta_G = (((maximum - G) / 6) + (delta / 2)) / delta
+    delta_B = (((maximum - B) / 6) + (delta / 2)) / delta
+
+    H = delta_B - delta_G
+    H = np.where(G == maximum, (1 / 3) + delta_R - delta_B, H)
+    H = np.where(B == maximum, (2 / 3) + delta_G - delta_R, H)
+    H[np.asarray(H < 0)] += 1
+    H[np.asarray(H > 1)] -= 1
+    H[np.asarray(delta == 0)] = 0
+
+    HSL = tstack([H, S, L])
+
+    return from_range_1(HSL)
+
+
+RGB = np.array([0.99603944, 0.87347144, 0.24350795])
+print(HCL_to_RGB(RGB))
