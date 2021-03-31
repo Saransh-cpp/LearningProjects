314b574a4130355ded52ffb558fe676f2c1769e1
Saransh <saransh0701@gmail.com>
2021-03-31 13:19:28 +0530

    Simulating drive cycles as pybamm experiments

--- python/PyBamm/issue-1193.py
+++ python/PyBamm/issue-1193.py
@@ -5,22 +5,22 @@ import numpy as np
 model = pybamm.lithium_ion.DFN()
 params = model.default_parameter_values
 
-# drive_cycle = pd.read_csv("US06.csv", comment="#", header=None).to_numpy()
+drive_cycle = pd.read_csv("US06.csv", comment="#", header=None).to_numpy()
 
-# experiment = pybamm.Experiment(["Run US06 for 1 hour"], drive_cycles={"US06": drive_cycle}, period="20 seconds")
+experiment = pybamm.Experiment(operating_conditions=["Run US06 for 1 hour"], drive_cycles={"US06": drive_cycle}, period="20 seconds")
 
-# sim = pybamm.Simulation(model, experiment=experiment, solver=pybamm.CasadiSolver(mode="fast"))
+sim = pybamm.Simulation(model, experiment=experiment, solver=pybamm.CasadiSolver(mode="fast"))
 
-# sim.solve()
-# solution = sim.solution
-
-# pybamm.dynamic_plot(solution)
-
-params["Current function [A]"] = 3
-
-sim = pybamm.Simulation(model)
-sim.solve([0, 3600])
+sim.solve()
 solution = sim.solution
 
 pybamm.dynamic_plot(solution)
+
+# params["Current function [A]"] = 3
+
+# sim = pybamm.Simulation(model)
+# sim.solve([0, 3600])
+# solution = sim.solution
+
+# pybamm.dynamic_plot(solution)
 
