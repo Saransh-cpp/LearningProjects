1172d1dd94e5e6f29768495f9b92966e109a6dce
Saransh <saransh0701@gmail.com>
2021-04-27 22:36:19 +0530

    issue-1465

--- python/PyBamm/issue-1465.py
+++ python/PyBamm/issue-1465.py
@@ -2,7 +2,5 @@ import pybamm
 
 params = pybamm.ParameterValues(chemistry=pybamm.parameter_sets.Chen2020)
 x = pybamm.linspace(3000,6000,100)
-params.plot(values=["Negative electrode exchange-current density [A.m-2]", "Electrolyte conductivity [S.m-1]"])
-
-
-
+print(params["Current function [A]"])
+params.plot(values=["Negative electrode exchange-current density [A.m-2]", "Electrolyte conductivity [S.m-1]"], eval=(1000, x, 300))
