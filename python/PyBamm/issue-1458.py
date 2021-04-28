488dbd4c7852aae00dec4bdadf55707711c28540
Saransh <saransh0701@gmail.com>
2021-04-29 00:51:34 +0530

    parameterization

--- python/PyBamm/issue-1458.py
+++ python/PyBamm/issue-1458.py
@@ -2,48 +2,48 @@ import numpy as np
 import pybamm
 from pprint import pprint
 
-full_model = pybamm.BaseModel(name="full model")
+# full_model = pybamm.BaseModel(name="full model")
 
-R = pybamm.Parameter("Particle radius [m]")
-D = pybamm.Parameter("Diffusion coefficient [m2.s-1]")
-j = pybamm.Parameter("Interfacial current density [A.m-2]")
-F = pybamm.Parameter("Faraday constant [C.mol-1]")
-c0 = pybamm.Parameter("Initial concentration [mol.m-3]")
+# R = pybamm.Parameter("Particle radius [m]")
+# D = pybamm.Parameter("Diffusion coefficient [m2.s-1]")
+# j = pybamm.Parameter("Interfacial current density [A.m-2]")
+# F = pybamm.Parameter("Faraday constant [C.mol-1]")
+# c0 = pybamm.Parameter("Initial concentration [mol.m-3]")
 
-c = pybamm.Variable("Concentration [mol.m-3]", domain="negative particle")
-c_av = pybamm.Variable("Average concentration [mol.m-3]")
+# c = pybamm.Variable("Concentration [mol.m-3]", domain="negative particle")
+# c_av = pybamm.Variable("Average concentration [mol.m-3]")
 
-N = -D * pybamm.grad(c)  # flux
-dcdt = -pybamm.div(N)
-full_model.rhs = {c: dcdt} 
+# N = -D * pybamm.grad(c)  # flux
+# dcdt = -pybamm.div(N)
+# full_model.rhs = {c: dcdt}
 
-full_model.initial_conditions = {c: c0}
+# full_model.initial_conditions = {c: c0}
 
-lbc = pybamm.Scalar(0)
-rbc = -j / F / D
-full_model.boundary_conditions = {c: {"left": (lbc, "Neumann"), "right": (rbc, "Neumann")}}
+# lbc = pybamm.Scalar(0)
+# rbc = -j / F / D
+# full_model.boundary_conditions = {c: {"left": (lbc, "Neumann"), "right": (rbc, "Neumann")}}
 
-full_model.variables = {
-    "Concentration [mol.m-3]": c,
-    "Surface concentration [mol.m-3]": pybamm.surf(c),
-    "Average concentration [mol.m-3]": pybamm.r_average(c),
-}
+# full_model.variables = {
+#     "Concentration [mol.m-3]": c,
+#     "Surface concentration [mol.m-3]": pybamm.surf(c),
+#     "Average concentration [mol.m-3]": pybamm.r_average(c),
+# }
 
-param = pybamm.ParameterValues(
-    {
-        "Particle radius [m]": 10e-6,
-        "Diffusion coefficient [m2.s-1]": 3.9e-14,
-        "Interfacial current density [A.m-2]": 1.4,
-        "Faraday constant [C.mol-1]": 96485,
-        "Initial concentration [mol.m-3]": 2.5e4,
-    }
-)
+# param = pybamm.ParameterValues(
+#     {
+#         "Particle radius [m]": 10e-6,
+#         "Diffusion coefficient [m2.s-1]": 3.9e-14,
+#         "Interfacial current density [A.m-2]": 1.4,
+#         "Faraday constant [C.mol-1]": 96485,
+#         "Initial concentration [mol.m-3]": 2.5e4,
+#     }
+# )
 
 # r = pybamm.SpatialVariable("r", domain=["negative particle"], coord_sys="spherical polar")
 # geometry = {"negative particle": {r: {"min": pybamm.Scalar(0), "max": R}}}
 # param.process_geometry(geometry)
 
-param.process_model("full model")
+# param.process_model("full model")
 
 # def my_current(t):
 #     return pybamm.sin(2 * np.pi * t / 60)
@@ -54,9 +54,29 @@ param.process_model("full model")
 
 # parameter_values["Current function [A]"] = my_current
 
-# model = pybamm.lithium_ion.SPMe()
+model = pybamm.lithium_ion.SPMe()
+model.parameters
 # sim = pybamm.Simulation(model, parameter_values=parameter_values)
 # t_eval = np.arange(0, 121, 1)
 # print(t_eval)
 # sim.solve([0, 3700])
 # sim.plot(["Current [A]", "Terminal voltage [V]"])
+
+values = {
+    "Particle radius [m]": 10e-6,
+    "Diffusion coefficient [m2.s-1]": 3.9e-14,
+    "Interfacial current density [A.m-2]": 1.4,
+    "Faraday constant [C.mol-1]": 96485,
+    "Initial concentration [mol.m-3]": 2.5e4,
+}
+
+param = pybamm.ParameterValues(values)
+print(param)
+
+param.update(
+    {
+        "Particle radius [m]": 10e-7,
+    }
+)
+
+print(param)
