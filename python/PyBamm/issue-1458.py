501200a3e1b8c86e1a7e221146bf7f990df3f656
Saransh <saransh0701@gmail.com>
2021-05-01 19:22:32 +0530

    Testing model.print_parameter_info()

--- python/PyBamm/issue-1458.py
+++ python/PyBamm/issue-1458.py
@@ -90,18 +90,97 @@ from pprint import pprint
 # sim.solve([0, 3600])
 # sim.plot()
 
-mydict = {
-    1: {
-        "negative tab": (2),
-        "positive tab": (3),
-    }
-}
+# mydict = {
+#     1: {
+#         "negative tab": (2),
+#         "positive tab": (3),
+#     }
+# }
 
-keys = mydict.values()
-print(keys)
-for key in keys:
-    if isinstance(key, dict):
-        if "positive tab" in key.keys():
-            side_list = ["positive tab", "negative tab", "no tab"]
+# keys = mydict.values()
+# print(keys)
+# for key in keys:
+#     if isinstance(key, dict):
+#         if "positive tab" in key.keys():
+#             side_list = ["positive tab", "negative tab", "no tab"]
 
-print(side_list)
+# print(side_list)
+
+# param = pybamm.ParameterValues(["a", 6])
+# param.update
+# import os
+
+# print(os.path.exists("graphite_LGM50_ocp_Chen2020.csv"))
+# load model
+# model = pybamm.lithium_ion.BasicDFN()
+# default_parameters = model.default_parameter_values
+
+# # Read my anode parameters from csv file
+# anode_param = pybamm.ParameterValues("parameters.csv")
+
+# # Update parameter values
+# orig_values = pybamm.ParameterValues(chemistry=pybamm.parameter_sets.Chen2020)
+# # print(orig_values)
+# orig_values.update(anode_param) 
+
+# print(orig_values == pybamm.parameter_sets.Chen2020)
+import unittest
+from unittest.mock import patch, call
+
+# class TestFunc(unittest.TestCase):
+#     @patch('builtins.print')
+#     def test(self, mocked_print):
+#         for cc_dimension in [0]:
+#             geometry = pybamm.battery_geometry(current_collector_dimension=cc_dimension)
+#         print(geometry.print_parameter_info())
+
+#         # print(end='\r')
+#         mocked_print.assert_called_with('Negative electrode thickness [m] (Parameter)')
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+# for cc_dimension in [0]:
+#     geometry = pybamm.battery_geometry(current_collector_dimension=cc_dimension)
+#     print(geometry.print_parameter_info(test=True))
+
+# model = pybamm.BaseModel()
+# a = pybamm.Parameter("a")
+# b = pybamm.InputParameter("b", "test")
+# c = pybamm.Parameter("c")
+# d = pybamm.Parameter("d")
+# e = pybamm.Parameter("e")
+# f = pybamm.InputParameter("f")
+# g = pybamm.Parameter("g")
+# h = pybamm.Parameter("h")
+
+# u = pybamm.Variable("u")
+# v = pybamm.Variable("v")
+# model.rhs = {u: -u * a}
+# model.algebraic = {v: v - b}
+# model.initial_conditions = {u: c, v: d}
+# model.events = [pybamm.Event("u=e", u - e)]
+# model.variables = {"v+f": v + f}
+# model.boundary_conditions = {
+#     u: {"left": (g, "Dirichlet"), "right": (0, "Neumann")},
+#     v: {"left": (0, "Dirichlet"), "right": (h, "Neumann")},
+# }
+
+# model.variables = {
+#     "v+f": v + pybamm.FunctionParameter("f", {"Time [s]": pybamm.t})
+# }
+# model.print_parameter_info()
+
+var = pybamm.Variable("var")
+model = pybamm.BaseModel()
+model.algebraic = {var: var + pybamm.InputParameter("value")}
+model.initial_conditions = {var: 2}
+
+        # create discretisation
+disc = pybamm.Discretisation()
+disc.process_model(model)
+
+        # Solve
+solver = pybamm.AlgebraicSolver()
+solution = solver.solve(model, np.linspace(0, 1, 10), inputs={"value": 7})
+model.print_parameter_info()
