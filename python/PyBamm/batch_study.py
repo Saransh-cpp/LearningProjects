9d656b04087f5cf57bcb100bd9473fe9cd835341
Saransh <saransh0701@gmail.com>
2021-06-03 21:23:53 +0530

    Batch study

--- python/PyBamm/batch_study.py
+++ python/PyBamm/batch_study.py
@@ -1,17 +1,24 @@
 import pybamm
 
+param1 = pybamm.ParameterValues(chemistry=pybamm.parameter_sets.Yang2017)
+param2 = pybamm.ParameterValues(chemistry=pybamm.parameter_sets.Yang2017)
+param3 = pybamm.ParameterValues(chemistry=pybamm.parameter_sets.Yang2017)
+
+param1["Current function [A]"] = 5
+param2["Current function [A]"] = 1.5
+param3["Current function [A]"] = 0.5
+
 model = pybamm.BatchStudy(
     permutations=True,
-    models={"0": pybamm.lithium_ion.DFN(),
-        "1": pybamm.lithium_ion.SPMe()
+    models={
+        "0": pybamm.lithium_ion.DFN(),
     },
-    )
+    parameter_values={
+        "param1": param1,
+        "param2": param2,
+        "param3": param3
+    }
+)
 
-model.solve(t_eval=[0, 3600])
+model.solve(t_eval=[0, 3700])
 model.plot()
-
-model = pybamm.lithium_ion.Yang2017()
-
-sim = pybamm.Simulation(model)
-sim.solve([0, 3700])
-sim.plot()
