860ab4d775564f68009dcddfe87e50a2c199a055
Saransh <saransh0701@gmail.com>
2021-08-25 19:18:33 +0530

    Add KNN classifier using sklearn

--- python/Machine Learning/AndrewNgInPython/Classifier/knn_classifier_sklearn.py
+++ python/Machine Learning/AndrewNgInPython/Classifier/knn_classifier_sklearn.py
@@ -1,2 +1,16 @@
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
+
+data = datasets.load_iris()
+
+classifier = KNeighborsClassifier()
+classifier.fit(data['data'][:-30], data['target'][:-30])
+
+predicted_category = classifier.predict(data['data'][-30:])
+
+# comparing predicted and original labels
+print(
+    f"\nPredicted labels - {predicted_category}\n\n\nOriginal labels - {data['target'][-30:]}\n\n"
+)
+
+print(f"Prdicted value: {classifier.predict([data['data'][-100]])} | Original value - {data['target'][-100]}")
