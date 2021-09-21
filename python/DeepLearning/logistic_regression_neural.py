1c75786a2d63dc8cdcccc69ddbe0e576fc442f84
Saransh <saransh0701@gmail.com>
2021-09-22 00:42:31 +0530

    Create basic logistic regression with backprop

---
+++ python/DeepLearning/logistic_regression_neural.py
@@ -0,0 +1,64 @@
+import numpy as np
+from sklearn import datasets
+
+
+class LogisticRegression:
+    def __init__(self, X, Y, alpha=0.05):
+        self.X = X
+        self.Y = Y
+        self.alpha = alpha
+        self.m = len(self.X)
+
+    def fit(self):
+        J = 0
+        J_last = 1
+        dW = np.zeros(shape=(self.m, 1))
+        self.b = 0
+        self.W = np.zeros(shape=(self.m, 1))
+
+        while True:
+            Z = np.dot(self.W.T, self.X) + self.b
+            A = np.array([self.sigmoid(x) for x in Z])
+            dZ = A - self.Y
+            dW = (1 / self.m) * (np.dot(self.X, dZ.T))
+            db = (1 / self.m) * np.sum(dZ)
+
+            J = -np.sum(
+                np.multiply(self.Y.T, np.array([np.log(x) for x in A.T]))
+                + np.multiply(1 - self.Y.T, np.array([np.log(1 - x) for x in A.T]))
+            )
+
+            self.W = self.W - self.alpha * dW
+            self.b = self.b - self.alpha * db
+
+            print(J)
+
+            if abs(J - J_last) < 1e-3:
+                break
+            else:
+                J_last = J
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def predict(self, x):
+        prediction = []
+
+        for single_data in x:
+            print(self.sigmoid(np.dot(single_data, self.W) + self.b))
+            prediction.append(
+                1 if self.sigmoid(np.dot(single_data, self.W) + self.b) > 0.5 else 0
+            )
+
+        return prediction
+
+
+if __name__ == "__main__":
+    data = datasets.load_iris()
+
+    x_train = data.data[:-60]
+    y_train = data.target[:-60]
+
+    model = LogisticRegression(x_train.T, np.array([y_train]))
+    model.fit()
+    print(model.predict(np.array([data.data[-100]])), data.target[-100])
