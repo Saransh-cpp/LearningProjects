03b745ba61a9a38c575b48e4b498c04d7e7f0ed8
Saransh <saransh0701@gmail.com>
2021-10-01 17:23:37 +0530

    Try OCRing bills

--- python/OCR/ocrpackage/test_bills.py
+++ python/OCR/ocrpackage/test_bills.py
@@ -1,10 +1,30 @@
+import nltk
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+import cv2
+import pytesseract
 import aiview_ocr
 
-# help(aiview_ocr.OCR)
-ocr = aiview_ocr.OCR(
-    is_scanned=False,
-    path="IMG_20210925_195026.jpg",
-    tesseract_location=r"D:\Saransh\Softwares\Tesseract-OCR\tesseract.exe"
-)
 
-ocr.ocr()
+# ocr = aiview_ocr.OCR(
+#     True, # is_scanned
+#     r"CosmosOne.jpg", # path
+#     r"D:\Saransh\Softwares\Tesseract-OCR\tesseract.exe", # tesseract_location
+# )
+
+pre = aiview_ocr.Preprocessor("CosmosOne.jpg")
+pre.rotate(save=True)
+
+
+# match=re.findall(r'\d+[/.-]\d+[/.-]\d+', ocr.text)
+
+# st=" "
+# st=st.join(match)
+# print(st)
+
+# price=re.findall(r'[\$\£\€](\d+(?:\.\d{1,2})?)',ocr.text)
+# price = list(map(float,price))
+# print(price)
+# print(max(price))
+# x=max(price)
