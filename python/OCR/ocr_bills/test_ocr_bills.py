516bc5e3d7b093ad07c0861c79df4fbd347ffa77
Saransh <saransh0701@gmail.com>
2021-10-04 19:19:57 +0530

    Improve preprocessing

--- python/OCR/ocr_bills/test_ocr_bills.py
+++ python/OCR/ocr_bills/test_ocr_bills.py
@@ -24,14 +24,24 @@ def thick_font(image):
 
     image = cv2.bitwise_not(image)
     kernel = np.ones((2, 2), np.uint8)
-    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.dilate(image, kernel, iterations=3)
     image = cv2.bitwise_not(image)
     return image
+
+def thin_font(image):
+    import numpy as np
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2,2),np.uint8)
+    image = cv2.erode(image, kernel, iterations=3)
+    image = cv2.bitwise_not(image)
+    return (image)
 
 
 pre = Preprocessor("Bill3.jpg")
 
 img = pre.scan(save=True)
+
+orig = img.copy()
 img = noise_removal(img)
 
 img = thick_font(img)
@@ -50,40 +60,45 @@ lines = cv2.HoughLinesP(
 angles = []
 for [[x1, y1, x2, y2]] in lines:
     # drawing Hough lines
-    angle = 90 + math.degrees(math.atan2(y2 - y1, x2 - x1))
+    angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
     angles.append(angle)
 
 # median angle
 median_angle = np.median(angles)
-print(median_angle)
+
 # actual rotate
-img = ndimage.rotate(img, median_angle)
+img = ndimage.rotate(orig, median_angle)
+# img = noise_removal(img)
 
-cv2.imwrite("rotated.png", img)
+cv2.imwrite("rotated1.png", img)
 
-tesseract_location = r"D:\Saransh\Softwares\Tesseract-OCR\tesseract.exe"
+# img = thin_font(img)
 
-pytesseract.pytesseract.tesseract_cmd = tesseract_location
+# cv2.imwrite("processed.png", img)
 
-# reading the image
-img = cv2.imread("rotated.png")
+# tesseract_location = r"D:\Saransh\Softwares\Tesseract-OCR\tesseract.exe"
 
-# extracting the text
-text = pytesseract.image_to_string(img, config="-l eng --oem 1 --psm 11")
-print(text)
+# pytesseract.pytesseract.tesseract_cmd = tesseract_location
 
-# adding boxes around the words
-boxes = pytesseract.image_to_data(img)
-for z, box in enumerate(boxes.splitlines()):
-    if z != 0:
-        box = box.split()
+# # reading the image
+# img = cv2.imread("rotated.png")
 
-        # if the data has a word
-        if len(box) == 12:
+# # extracting the text
+# text = pytesseract.image_to_string(img, config="-l eng --oem 1 --psm 11")
+# print(text)
 
-            x, y = int(box[6]), int(box[7])
-            h, w = int(box[8]), int(box[9])
+# # adding boxes around the words
+# boxes = pytesseract.image_to_data(img)
+# for z, box in enumerate(boxes.splitlines()):
+#     if z != 0:
+#         box = box.split()
 
-            cv2.rectangle(img, (x, y), (x + h, y + w), (0, 0, 255), 1)
+#         # if the data has a word
+#         if len(box) == 12:
 
-cv2.imwrite("OCR.png", img)
+#             x, y = int(box[6]), int(box[7])
+#             h, w = int(box[8]), int(box[9])
+
+#             cv2.rectangle(img, (x, y), (x + h, y + w), (0, 0, 255), 1)
+
+# cv2.imwrite("OCR.png", img)
