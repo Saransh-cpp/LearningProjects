6abb6609b86def868c5c553a9d4d1c6493a9f04b
Saransh <saransh0701@gmail.com>
2021-10-09 02:50:00 +0530

    OCR invoices done

--- python/OCR/ocr_bills/test_ocr_bills.py
+++ python/OCR/ocr_bills/test_ocr_bills.py
@@ -1,18 +1,14 @@
-import cv2
-import math
-import numpy as np
-import pytesseract
-from scipy import ndimage
 import aiview_ocr
 
 
 ocr = aiview_ocr.OCR(
     True,
     # "bills/1174-receipt.jpg",
+    # "bills/1166-receipt.jpg",
     # "bills/1000-receipt.jpg",
     # "bills/1178-receipt.jpg",
-    "bills/1146-receipt.jpg",
-    # "bills/1132-receipt.jpg",
+    # "bills/1146-receipt.jpg",
+    "bills/1132-receipt.jpg",
     # r"D:\Saransh\Softwares\Tesseract-OCR\tesseract.exe"
 )
 
@@ -21,4 +17,3 @@ text = ocr.ocr_sparse_text(languages=["en"])
 print(text)
 
 print(ocr.process_extracted_text_from_invoice())
-
