df278c1d3e5dbf0e51489c52d59a71e492d697b1
Saransh <saransh0701@gmail.com>
2021-11-14 00:19:43 +0530

    Neumann BCs in telegraph equation

--- python/NNPDEs/telegraph.py
+++ python/NNPDEs/telegraph.py
@@ -10,14 +10,16 @@ a = 0.1
 L = 1
 n = 1
 
+
 def pde(x, u):
     """
-    Expresses the PDE residual of the telegraph equation.        
+    Expresses the PDE residual of the telegraph equation.
     """
     u_t = dde.grad.jacobian(u, x, i=0, j=1)
     u_xx = dde.grad.hessian(u, x, i=0, j=0)
     u_tt = dde.grad.hessian(u, x, i=0, j=1)
     return u_tt + (a * u_t) + (b * u) - ((c ** 2) * u_xx)
+
 
 # Computational geometry:
 geom = dde.geometry.Interval(0, L)
@@ -26,12 +28,25 @@ geomtime = dde.geometry.GeometryXTime(geom, timedomain)
 
 # Initial and boundary conditions:
 bc = dde.DirichletBC(geomtime, lambda x: 0, lambda _, on_boundary: on_boundary)
+# bc_r = dde.NeumannBC(geom, lambda X: 0, lambda _, on_boundary: on_boundary)
 ic = dde.IC(
-    geomtime, lambda x: np.sin(n*np.pi*x[:, 0:1]/L), lambda _, on_initial: on_initial
+    geomtime,
+    lambda x: np.sin(n * np.pi * x[:, 0:1] / L),
+    lambda _, on_initial: on_initial,
 )
 
 data = dde.data.TimePDE(
-    geomtime, pde, [bc, ic], num_domain=2540, num_boundary=80, num_initial=160, num_test=2540
+    geomtime,
+    pde,
+    [
+        bc,
+        # bc_r,
+        ic
+    ],
+    num_domain=2540,
+    num_boundary=80,
+    num_initial=160,
+    num_test=2540,
 )
 net = dde.nn.FNN([2] + [20] * 3 + [1], "tanh", "Glorot normal")
 model = dde.Model(data, net)
